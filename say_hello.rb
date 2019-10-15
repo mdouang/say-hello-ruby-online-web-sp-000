df4c0ce79b5f21ac54614abe49d3db34734ca62a
Maikaru <maikaru@maikaru.com>
2019-10-15 20:12:15 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,5 @@
-# Build your say_hello method here
+greeting("Sophie", "Ember.js")
+# > Hello, Sophie. We heard you are a great Ember.js programmer. 
+ 
+greeting("Dan")
+# > Hello, Dan. We heard you are a great Ruby programmer.
